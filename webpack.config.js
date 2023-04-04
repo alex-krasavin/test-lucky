585017a4949cb84b8889293d12c163ec5d6b9f6a
Aleksandr Krasavin <krasa19920906@gmail.com>
2023-04-04 18:02:30 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    output: {
+    filename: 'main.min.js',
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: "/node_modules/",
+            loader: 'babel-loader',
+        }]
+    },
+    // presets:[
+    //     "env"
+    // ]
+}
