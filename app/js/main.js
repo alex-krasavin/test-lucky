00ab6985565113466517b2d5f56cc71241227675
Aleksandr Krasavin <krasa19920906@gmail.com>
2023-04-05 18:58:09 +0300

    created adaptive design and created validate

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
+"use strict"
+
 window.addEventListener("DOMContentLoaded",() => {
     setTimeout(initAnimate,3000);
+    initMenu();
+    validateSearch();
 });
 
 function initAnimate () {
@@ -9,4 +13,43 @@ function initAnimate () {
     })
 }
 
+function initMenu () {
+    const btnMenu = document.querySelector(".mob-menu__button");
+    const mobMenu = document.querySelector(".mob-menu");
+
+    const toggleActiveClassMenu = () => {
+        btnMenu.classList.toggle("active-menu");
+        mobMenu.classList.toggle("active");
+    }
+
+    btnMenu.addEventListener("click",() => {
+        toggleActiveClassMenu();
+    })
+
+    mobMenu.addEventListener("click",(e) => {
+        if(e.target.classList.contains("nav__link")) {
+            toggleActiveClassMenu();
+        }
+    })
+}
+
+function validateSearch () {
+    const inputSearch = document.querySelector("#search");
+    const search = document.querySelector(".hero__search");
+    
+    inputSearch.addEventListener("input",() => {
+        const value = inputSearch.value.replace(/[!,@,#,$,%,^,&,*,()]/g,"");
+        inputSearch.value = value;
+
+        if(inputSearch.value.length > 12) {
+            inputSearch.value = inputSearch.value.substring(0,12);
+        }
+
+        if(inputSearch.value.length < 4 && inputSearch.value.length !== 0) {
+            search.classList.add("no-validate")
+        }else {
+            search.classList.remove("no-validate")
+        }
+    })
+}
 
